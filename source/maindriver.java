d89e165cdfc1f43c02746e34bce925cbf4f738d7
EvanKA <evanka2133@yahoo.com>
2015-07-13 14:52:49 +0700

    packing to folder

---
+++ source/maindriver.java
@@ -0,0 +1,9 @@
+public class maindriver
+{
+  public static void main(String[] args)
+  {
+	  mainmenu mainFrame = new mainmenu();
+	  mainFrame.setVisible(true);
+	  mainFrame.setSize(300,300);
+  }
+}
