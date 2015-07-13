85cb85cbe53e45d9b8ef51e862a12ad1db3a7846
EvanKA <evanka2133@yahoo.com>
2015-07-13 15:04:10 +0700

    Delete maindriver.java

--- maindriver.java
+++
@@ -1,9 +0,0 @@
-public class maindriver
-{
-  public static void main(String[] args)
-  {
-	  mainmenu mainFrame = new mainmenu();
-	  mainFrame.setVisible(true);
-	  mainFrame.setSize(300,300);
-  }
-}
